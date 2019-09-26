53f85ca73c965a9415b298964c8279d919c044cc
davidcGIThub <davidc@linuxpopper.org>
2019-09-26 17:48:19 -0600

    part b of hw 3 math signals

--- Math_Signals_Systems/homework3/problem_1.m
+++ Math_Signals_Systems/homework3/problem_1.m
@@ -1,3 +1,4 @@
+%problem 1
 t = linspace(-1,1,10000);
 p = zeros(10000, 5);
 q = p;
@@ -16,7 +17,7 @@ for ii = 2:5
     end
     q(:,ii) = e(:,ii) ./ sqrt(sum(e(:,ii).*e(:,ii).*dt));
 end
-
+figure(1);
 plot(t,q(:,1));
 hold on;
 plot(t,q(:,2));
@@ -24,3 +25,27 @@ plot(t,q(:,3));
 plot(t,q(:,4));
 plot(t,q(:,5));
 legend('q0','q1','q2','q3','q4');
+hold off;
+
+% problem 2
+f = transpose(exp(-t));
+
+R = zeros(5,5);
+for ii = 1:5
+    for jj = 1:5
+        R(ii,jj) = sum(q(:,ii) .* q(:,jj) * dt);
+    end
+end
+
+P = zeros(5,1);
+for ii = 1:5
+    P(ii,1) = sum(f .* q(:,ii) * dt);
+end
+
+x_hat = q*inv(R)*P;
+
+figure(2)
+plot(t,x_hat);
+hold on;
+plot(t,f);
+legend('approximation', 'exp(-t)');
