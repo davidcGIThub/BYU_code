9e330c678c84f2a2430c1a996cbfaa1635c1e24c
davidcGIThub <davidc@linuxpopper.org>
2019-10-08 10:24:30 -0600

    dynamic landmarks

--- Autonomous_Systems/UnscentedKalmanFilter/homework3.py
+++ Autonomous_Systems/UnscentedKalmanFilter/homework3.py
@@ -36,13 +36,15 @@ sig_b = 0.05;
 Sig = np.array([[1,0,0],
                [0,1,0],
                [0,0,0.1]]);
-landmarks = np.array([[6,4]]);#,[-7,8],[6,-4]]);
+landmarks = np.array([[6,4],[-7,8],[6,-4]]);
 len_m = np.size(landmarks,0);
-#Initialize Objects
+
+#Initialize Estimation Objects
 rb = rbm(x0,y0,theta0,alpha1,alpha2,alpha3,alpha4,dt);
 rb_est = rbm(x0,y0,theta0,alpha1,alpha2,alpha3,alpha4,dt);
 m = lmm(landmarks , sig_r , sig_b);
 ukf = UKF(dt,alpha,sig_r,sig_b,alfa,kappa,beta);
+
 #initialize figures
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
@@ -51,38 +53,39 @@ ax.grid();
 robot_fig = plt.Polygon(rb.getPoints(),fc = 'g');
 robot_est_fig = plt.Polygon(rb_est.getPoints(),fill=False);
 time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes);
-lmd_fig1 = plt.Circle(landmarks[0], radius = 0.5, fc = 'b');
-#lmd_fig2 = plt.Circle(landmarks[1], radius = 0.5, fc = 'b');
-#lmd_fig3 = plt.Circle(landmarks[2], radius = 0.5, fc = 'b');
+ms = 12;
+lmd_figs, = ax.plot([],[], 'bo', ms=ms); 
+lmd_meas_figs, = ax.plot([],[], 'ko', fillstyle = 'none', ms=ms); 
 
 def init():
     #initialize animation
     ax.add_patch(robot_fig);
     ax.add_patch(robot_est_fig);
     time_text.set_text('0.0');
-    ax.add_patch(lmd_fig1);
-    #ax.add_patch(lmd_fig2);
-    #ax.add_patch(lmd_fig3);
-    return robot_fig, robot_est_fig, time_text, lmd_fig1#, lmd_fig2, lmd_fig3
+    lmd_figs.set_data(landmarks[:,0],landmarks[:,1]);
+    lmd_meas_figs.set_data([],[]);
+    return robot_fig, robot_est_fig, time_text, lmd_figs, lmd_meas_figs
 
 def animate(i):
-    global rb, rb_est, m, t, vc, wc, mu, Sig
+    global rb, rb_est, m, t, vc, wc, mu, Sig, ms
     #propogate robot motion
     u = np.array([vc[i],wc[i]]);
     rb.vel_motion_model(u);
     robot_fig.xy  = rb.getPoints();
     state = rb.getState();
     #measure landmark position
-    #landmark_pos = m.getLandmarks(state);
+    landmarks_meas = m.getLandmarks(state);
     Ranges = m.getRanges(state);
     Bearings = m.getBearings(state);
     z = np.array([Ranges.flatten(), Bearings.flatten()]);
+    lmd_meas_figs.set_data(landmarks_meas[:,0], landmarks_meas[:,1]);
+    lmd_meas_figs.set_markersize(ms);
     #estimate robot motion
     #(mu, Sig, landmarks_meas)  = ekf.EKF_Localization(mu,Sig,u,state);
     #rb_est.setState(mu[0],mu[1],mu[2]);
     robot_est_fig.xy  = rb.getPoints();
     #update time
-    time_text.set_text('time = %.1f' % t[i])
+    time_text.set_text('time = %.1f' % t[i]);
     #save state information
     x_true[i] = state[0];
     y_true[i] = state[1];
@@ -90,12 +93,12 @@ def animate(i):
     x_est[i] = mu[0];
     y_est[i] = mu[1];
     theta_est[i] = mu[2];
-    return robot_fig, robot_est_fig, time_text, lmd_fig1#, lmd_fig2, lmd_fig3
+    return robot_fig, robot_est_fig, time_text, lmd_figs, lmd_meas_figs
 
 from time import time
 animate(0);
 
 ani = animation.FuncAnimation(fig, animate, frames = np.size(t), 
-                            interval = dt*100, blit = True, init_func = init, repeat = False)
+                            interval = dt*1000, blit = True, init_func = init, repeat = False)
 
 plt.show();
