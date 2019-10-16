e60f1c96f8aedda3f866d0ac67a743c5452191c0
davidcGIThub <davidc@linuxpopper.org>
2019-10-16 14:02:55 -0600

    visuals done ogm

--- Autonomous_Systems/OccupancyGridMapping/RobotPath.py
+++ Autonomous_Systems/OccupancyGridMapping/RobotPath.py
@@ -6,25 +6,27 @@ class RobotPath:
     def __init__(self, 
                  x_path,
                  y_path,
-                 th_path):
+                 th_path,
+                 bot_len = 1.0):
         self.x_path = x_path
         self.y_path = y_path
         self.th_path = th_path
+        self.bot_len = bot_len
 
     def getState(self,index):
-        x = self.x[index]
-        y = self.y[index]
-        th = self.th[index]
+        x = self.x_path[index]
+        y = self.y_path[index]
+        th = self.th_path[index]
         return np.array([x,y,th])
 
-    def getPoints(self,index):
-        x = self.x[index]
-        y = self.y[index]
-        th = self.th[index]
+    def getPoints(self,index,size_factor):
+        x = self.x_path[index]
+        y = self.y_path[index]
+        th = self.th_path[index]
         R = np.array([[np.cos(th), -np.sin(th)],
                       [np.sin(th), np.cos(th)]])
-        xy = np.array([[-1, 1, -1],
-                       [.5, 0, -0.5]])
+        xy = np.array([[-self.bot_len, self.bot_len, -self.bot_len],
+                       [self.bot_len/2, 0, -self.bot_len/2]]) * size_factor
         xy = np.dot(R,xy)
-        xy = xy + np.array([[x],[y]])
+        xy = xy + np.array([[x],[y]])*size_factor
         return np.transpose(xy)
--- Autonomous_Systems/OccupancyGridMapping/homework6.py
+++ Autonomous_Systems/OccupancyGridMapping/homework6.py
@@ -1,21 +1,30 @@
 import numpy as np
 import cv2
 from RobotPath import RobotPath
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
 
+matdata = loadmat('state_meas_data.mat')
+X = matdata['X']
+x_data = X[0]
+y_data = X[1]
+th_data = X[2]
 
 width = 100
 height = 100
-FPS = 24
-seconds = 2
+FPS = 1000
+resize_factor = 10
 
-rp  = RobotPath()
+rp  = RobotPath(x_data,y_data,th_data)
 
-for _ in range(FPS*seconds):
-    frame = np.random.randint(0, 256, 
-                              (height, width), 
-                              dtype=np.uint8)
-    enlarged_frame = np.repeat(np.repeat(frame,10,axis=0),10,axis=1)
-    pts = np.array([[[500,500],[600,500],[550,600]]], dtype = np.int32)
-    cv2.fillPoly(enlarged_frame,pts,0)
-    cv2.imshow('Image',enlarged_frame)
-    cv2.waitKey(int(1/FPS*1000)) 
+for i in range(np.size(X,1)):
+    #frame = np.random.randint(0, 256, 
+    #                          (height, width), 
+    #                          dtype=np.uint8)
+    frame = np.zeros([height,width],dtype=np.uint8)
+    enlarged_frame = np.repeat(np.repeat(frame,resize_factor,axis=0),resize_factor,axis=1)
+    pts = np.array([rp.getPoints(i,resize_factor)], dtype = np.int32)
+    cv2.fillPoly(enlarged_frame,pts,255)
+    flipped = cv2.flip(enlarged_frame,0)
+    cv2.imshow('Image',flipped)
+    cv2.waitKey(int(1.0/FPS*1000)) 
