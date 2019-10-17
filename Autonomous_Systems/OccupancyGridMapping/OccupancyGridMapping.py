f9388a7149db7a9064748ba41802101bb0c0fe5a
davidcGIThub <davidc@linuxpopper.org>
2019-10-17 00:12:05 -0600

    finished occupancy grid mapping

--- Autonomous_Systems/OccupancyGridMapping/OccupancyGridMapping.py
+++ Autonomous_Systems/OccupancyGridMapping/OccupancyGridMapping.py
@@ -4,13 +4,13 @@ import numpy as np
 
 #p(m_i) = 0.6 to 0.7 if hit detected
 #p(m_i) = 0.3 to 0.4 if no hit
-class ogm:
+class OGM:
     
     def __init__(self, 
-                alpha = 1.0, #m
-                beta = 5.0/180*np.pi, #rad
-                z_max = 150.0, #m
-                pm_hit = 0.7, #probability of mass if hit detected
+                alpha = 1.0,            #m
+                beta = 5.0/180*np.pi,   #rad
+                z_max = 150.0,          #m
+                pm_hit = 0.7,           #probability of mass if hit detected
                 height = 100,
                 width = 100):
 
@@ -25,7 +25,7 @@ class ogm:
         self.height = height
         self.width = width
         self.lt_map = np.zeros([height,width]) + self.lo
-        self.map = np.zeros([height,width])
+        self.map = np.zeros([height,width]) + 1 - 1/(1+np.exp(self.lo))
 
     def inverse_range_sensor_model(self,m,X,z):
         mx = m[0]
@@ -34,21 +34,28 @@ class ogm:
         y = X[1]
         th = X[2]
 
-        r = np.sqrt((mx - x)**2 + (my-y)**2))   #range
-        phi = arctan2(my-y,mx-x) - th           #bearing
+        r = np.sqrt((mx - x)**2 + (my-y)**2)   #range
+        phi = np.arctan2(my-y,mx-x) - th       #bearing
         k = np.argmin(np.abs(phi - z[1,:]))
         r_k = z[0,k]
         phi_k = z[1,k]
         if r > np.min([self.z_max , r_k + self.alpha/2]) or np.abs(phi-phi_k) > self.beta/2:
             l = self.lo
-        elif r_k < z_max and np.abs(r - r_k) < self.alpha/2.0:
+        elif r_k < self.z_max and np.abs(r - r_k) < self.alpha/2.0:
             l = self.locc
         elif r <= r_k:
             l = self.lfre
+        else:
+            l = self.lo
         return l
 
-    def occupancy_Grid_Mapping(self,x,z):
-        for i in range(0:self.height):
-            for j in range(0:self.width):
-                lt_map[i,j] = lt_map[i,j] + self.inverse_range_sensor_model(   )  - self.lo
-        
+    def occupancy_Grid_Mapping(self,X,z):
+        for i in range(0,self.height):
+            for j in range(0,self.width):
+                m = np.array([j+1,i+1])
+                self.lt_map[i,j] = self.lt_map[i,j] + self.inverse_range_sensor_model(m,X,z)  - self.lo
+                self.map[i,j] = 1-1/(1+np.exp(self.lt_map[i,j]))
+        return self.map
+    
+    #def getMap(self):
+    #    return 1-1/(1+np.exp(self.lt_map))
--- Autonomous_Systems/OccupancyGridMapping/homework6.py
+++ Autonomous_Systems/OccupancyGridMapping/homework6.py
@@ -2,13 +2,18 @@ import numpy as np
 import cv2
 from RobotPath import RobotPath
 from scipy.io import loadmat
-import matplotlib.pyplot as plt
+from OccupancyGridMapping import OGM
 
-matdata = loadmat('state_meas_data.mat')
+matdata = loadmat('BYU_code/Autonomous_Systems/OccupancyGridMapping/state_meas_data.mat')
 X = matdata['X']
+z = matdata['z']
 x_data = X[0]
 y_data = X[1]
 th_data = X[2]
+alpha = 1.0
+beta = 5.0/180*np.pi
+z_max = 150.0
+pm_hit = 0.7
 
 width = 100
 height = 100
@@ -16,15 +21,24 @@ FPS = 1000
 resize_factor = 10
 
 rp  = RobotPath(x_data,y_data,th_data)
+ogm = OGM(alpha, beta, z_max,pm_hit, height, width)
+last_image = np.array([[[]]])
 
 for i in range(np.size(X,1)):
     #frame = np.random.randint(0, 256, 
     #                          (height, width), 
     #                          dtype=np.uint8)
-    frame = np.zeros([height,width],dtype=np.uint8)
+    frame = ogm.occupancy_Grid_Mapping(X[:,i],z[:,:,i])
+    #frame = ogm.getMap()
+    #frame = np.zeros([height,width],dtype=np.uint8)
     enlarged_frame = np.repeat(np.repeat(frame,resize_factor,axis=0),resize_factor,axis=1)
     pts = np.array([rp.getPoints(i,resize_factor)], dtype = np.int32)
     cv2.fillPoly(enlarged_frame,pts,255)
     flipped = cv2.flip(enlarged_frame,0)
     cv2.imshow('Image',flipped)
-    cv2.waitKey(int(1.0/FPS*1000)) 
+    cv2.waitKey(int(1.0/FPS*1000))
+    if i == np.size(X,1)-1:
+        last_image = flipped
+
+cv2.imshow('Image',last_image)
+cv2.waitKey()
