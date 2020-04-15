33362d9a4c8114597cb08e091a1a9a372f5b3123
davidcGIThub <davidc@linuxpopper.org>
2020-04-15 04:51:48 -0600

    making progress on full state direct EKF

--- Flight_Dynamics_Control/python/project/fullStateDirectObserver.py
+++ Flight_Dynamics_Control/python/project/fullStateDirectObserver.py
@@ -14,13 +14,13 @@ from message_types.msg_state import msg_state
 class fullStateDirectObserver:
     def __init__(self, ts_control):
         self.estimated_state = msg_state()
-        self.estimated_state.pn = 0.      # inertial north position in meters
-        self.estimated_state.pe = 0.      # inertial east position in meters
-        self.estimated_state.h = 0.       # inertial altitude in meters
-        self.estimated_state.phi = 0.     # roll angle in radians
-        self.estimated_state.theta = 0.   # pitch angle in radians
-        self.estimated_state.psi = 0.     # yaw angle in radians
-        self.estimated_state.Va = 0.      # airspeed in meters/sec
+        self.estimated_state.pn = MAV.pn0      # inertial north position in meters
+        self.estimated_state.pe = MAV.pe0      # inertial east position in meters
+        self.estimated_state.h = -MAV.pd0       # inertial altitude in meters
+        self.estimated_state.phi = MAV.phi0    # roll angle in radians
+        self.estimated_state.theta = MAV.theta0   # pitch angle in radians
+        self.estimated_state.psi = MAV.psi0     # yaw angle in radians
+        self.estimated_state.Va = MAV.Va0      # airspeed in meters/sec
         self.estimated_state.alpha = 0.   # angle of attack in radians
         self.estimated_state.beta = 0.    # sideslip angle in radians
         self.estimated_state.p = 0.       # roll rate in radians/sec
@@ -40,23 +40,8 @@ class fullStateDirectObserver:
         self.estimated_state.p = self.lpf_gyro_x.update(measurements.gyro_x)
         self.estimated_state.q = self.lpf_gyro_y.update(measurements.gyro_y)
         self.estimated_state.r = self.lpf_gyro_z.update(measurements.gyro_z)
-        self.directEKF.update(self.estimated_state, measurements)
-        # self.gyro_x = 0  # gyroscope along body x axis
-        # self.gyro_y = 0  # gyroscope along body y axis
-        # self.gyro_z = 0  # gyroscope along body z axis
-        # self.accel_x = 0  # specific acceleration along body x axis
-        # self.accel_y = 0  # specific acceleration along body y axis
-        # self.accel_z = 0  # specific acceleration along body z axis
-        # self.mag_x = 0  # magnetic field along body x axis
-        # self.mag_y = 0  # magnetic field along body y axis
-        # self.mag_z = 0  # magnetic field along body z axis
-        # self.static_pressure = 0  # static pressure
-        # self.diff_pressure = 0  # differential pressure
-        # self.gps_n = 0  # gps north
-        # self.gps_e = 0  # gps east
-        # self.gps_h = 0  # gps altitude
-        # self.gps_Vg = 0  # gps ground speed
-        # self.gps_course = 0  # gps course angle
+        xhat = self.directEKF.update(self.estimated_state, measurements)
+        return self.estimated_state
 
 class alpha_filter:
     # alpha filter implements a simple low pass filter
@@ -71,7 +56,7 @@ class alpha_filter:
         return self.y
 
 class directExtendedKalmanFilter:
-    def __init__(self, x0 = np.array([[MAV.pn0, MAV.pe0, MAV.pd, MAV.Va0, MAV.v0, MAV.w0,
+    def __init__(self, x0 = np.array([[MAV.pn0, MAV.pe0, MAV.pd0, MAV.u0, MAV.v0, MAV.w0,
                                      MAV.phi0, MAV.theta0, MAV.psi0, 0, 0, 0,0,0]]).T ,
                     std_pn = SENS.gps_n_sigma**2,
                     std_pe = SENS.gps_e_sigma**2,
@@ -91,16 +76,20 @@ class directExtendedKalmanFilter:
         self.Ts = SIM.ts_control/self.N
         self.xhat = x0 #[pn, pe, pd, u ,v , w, phi, theta, psi, bx, by, bz, wn, we]
         self.P = np.identity(14)
-        self.Q_tune = np.identity()
+        self.Q_tune = np.identity() #tunable process noise
         np.fill_diagonal(self.Q_tune,[std_pn,std_pe,std_u,std_v,std_w,std_phi,std_theta,std_psi,
                                         std_bx,std_by,std_bz,std_wn,std_we])
-        self.Q_gyro = np.identity(3)
+        self.Q_gyro = np.identity(3) 
         np.fill_diagonal(self.Q_gyro, [SENS.gyro_sigma**2, SENS.gyro_sigma**2, SENS.gyro_sigma**2])
         self.Q_accel = np.identity(3)
         np.fill_diagonal(self.Q_accel, [SENS.gyro_accel**2, SENS.gyro_accel**2, SENS.gyro_accel**2])
-        self.R = np.identity(7)
-        np.fill_diagonal(self.R,[SENS.static_pres_sigma, SENS.diff_pres_sigma, SENS.beta_sigma, 
-                                 SENS.gps_n_sigma, SENS.gps_e_sigma, SENS.gps_Vg_sigma, SENS.gps_course_sigma_ave])
+        self.R = np.identity(7) #measurement noise
+        np.fill_diagonal(self.R,[SENS.static_pres_sigma**2, SENS.diff_pres_sigma**2, SENS.beta_sigma**2, 
+                                 SENS.gps_n_sigma**2, SENS.gps_e_sigma**2, SENS.gps_Vg_sigma**2, SENS.gps_course_sigma_ave**2])
+        self.gps_n_old = 9999
+        self.gps_e_old = 9999
+        self.gps_Vg_old = 9999
+        self.gps_course_old = 9999
     
     def update(self, est_state, measurements):
         #treat accel and gyro measurements as an input to the propagation
@@ -110,7 +99,7 @@ class directExtendedKalmanFilter:
         self.measurement_update(measurements)
         est_state.pn = self.xhat.item(0)
         est_state.pe = self.xhat.item(1)
-        est_state.h = self.xhat.item(2)
+        est_state.h = -self.xhat.item(2)
         est_state.u = self.xhat.item(3)
         est_state.v = self.xhat.item(4)
         est_state.w = self.xhat_item(5)
@@ -122,13 +111,30 @@ class directExtendedKalmanFilter:
         est_state.bz = self.xhat.item(11)
         est_state.wn = self.xhat.item(12)
         est_state.we = self.xhat.item(13)
+        #calculate remaining state estimates
+        V = np.array([[est_state.u,est_state.v,est_state.w]]).T
+        W = np.array([[est_state.wn,est_state.we,0]]).T
+        R = Euler2RotationMatrix(est_state.phi,est_state.theta,est_state.psi)
+        Vg = np.dot(R,V)
+        Va = V-np.dot(R.T,W)
+        ur = Va.item(0)
+        vr = Va.item(1)
+        wr = Va.item(2)
+        est_state.Vg = np.sqrt(Vg.item(0)**2 + Vg.item(1)**2)
+        est_state.Va = np.linalg.norm(Va)
+        est_state.alpha = np.arctan2(wr,ur)
+        est_state.beta = np.arcsin(vr/(est_state.Va))
+        est_state.gamma = 
+        est_state.chi = np.arctan2(Ve,Vn)
+
+        gamma, chi, Va, Vg , alpha
 
     def propagate_model(self, u_):
         for i in range(0, self.N):
              # propagate model
             self.xhat = self.xhat + self.Ts*self.f(self.xhat, u_)
             # compute Jacobian
-            A = jacobian(self.f,self.xhat,u_)
+            A = self.jacobian(self.f,self.xhat,u_)
             # compute G matrix for gyro noise
             u = self.xhat.item(3)
             v = self.xhat.item(4)
@@ -205,26 +211,66 @@ class directExtendedKalmanFilter:
         return _f
 
     def measurement_update(self, state, measurement):
-        # always update based on wind triangle pseudo measurement
-        # h = self.h(self.xhat, state)
-        # C = jacobian(self.h, self.xhat, state)
-        # y = np.array([measurement.gps_n, measurement.gps_e, measurement.gps_Vg, measurement.gps_course, 0, 0])
-        # for i in range(4, 6):
-        #     Ci = C[i][None,:]
-        #     Li = np.dot(self.P,Ci.T) * 1/(self.R[i,i] + np.dot(np.dot(Ci,self.P) , Ci.T))
-        #     temp = np.identity(7) - np.dot(Li,Ci)
-        #     self.P = np.dot(np.dot(temp , self.P)  ,  (temp).T) + np.dot(Li,self.R[i,i]*Li.T) 
-        #     self.xhat = self.xhat + np.dot(Li , (y[i] - h[i,0]))
+        # always update pressure measurements, and fake sideslip
+        h = self.h(self.xhat, state)
+        C = self.jacobian(self.h, self.xhat)
+        psuedo_vr = 0 #drive vr to zero so that beta = 0
+        y = np.array([[measurement.static_pressure, measurement.diff_pressure, psuedo_vr,
+                    measurement.gps_n, measurement.gps_e, measurement.gps_Vg, measurement.gps_chi]]).T
+        for i in range(0, 3):
+            Ci = C[i][None,:]
+            Li = np.dot(self.P,Ci.T) * 1/(self.R[i,i] + np.dot(np.dot(Ci,self.P) , Ci.T))
+            temp = np.identity(7) - np.dot(Li,Ci)
+            self.P = np.dot(np.dot(temp , self.P)  ,  (temp).T) + np.dot(Li,self.R[i,i]*Li.T) 
+            self.xhat = self.xhat + np.dot(Li , (y.item(i) - h.item(i)))
 
-        # # only update GPS when one of the signals changes
-        # if (measurement.gps_n != self.gps_n_old) \
-        #     or (measurement.gps_e != self.gps_e_old) \
-        #     or (measurement.gps_Vg != self.gps_Vg_old) \
-        #     or (measurement.gps_course != self.gps_course_old):
+        # only update GPS when one of the signals changes
+        if (measurement.gps_n != self.gps_n_old) \
+             or (measurement.gps_e != self.gps_e_old) \
+             or (measurement.gps_Vg != self.gps_Vg_old) \
+             or (measurement.gps_course != self.gps_course_old):
+            for i in range(3, 7):
+                Ci = C[i][None,:]
+                Li = np.dot(self.P,Ci.T) * 1/(self.R[i,i] + np.dot(np.dot(Ci,self.P) , Ci.T))
+                temp = np.identity(14) - np.dot(Li,Ci)
+                self.P = np.dot(np.dot(temp , self.P)  ,  temp.T) + np.dot(Li,self.R[i,i]*Li.T) 
+                self.xhat = self.xhat + np.dot(Li , (y.item(i) - h.item(i)))
+            # update stored GPS signals
+            self.gps_n_old = measurement.gps_n
+            self.gps_e_old = measurement.gps_e
+            self.gps_Vg_old = measurement.gps_Vg
+            self.gps_course_old = measurement.gps_course
 
-    def h(self,):
+    def h(self,x):
+        pn = x.item(0)
+        pe = x.item(1)
+        pd = -x.item(2)
+        u = x.item(3)
+        v = x.item(4)
+        w = x.item(5)
+        phi = x.item(6)
+        theta = x.item(7)
+        psi = x.item(8)
+        wn = x.item(12)
+        we = x.item(13)
+        V = np.array([[u,v,w]]).T
+        W = np.array([[wn,we,0]]).T
+        R = Euler2RotationMatrix(phi,theta,psi)
+        Vg = np.dot(R,V)
+        Vn = Vg.item(0) #north velocity
+        Ve = Vg.item(1) #east velocity
+        Va = V-np.dot(R.T,W)
+        vr = Va.item(1)
+        h_ = np.array([[-MAV.rho*MAV.gravity*pd],   #h_static
+                    [.5*MAV.rho*np.dot(Va.T,Va)],   #h_diff
+                    [vr],                           #h_beta - driving side dir Va zero
+                    [pn],                           #h_gps_n 
+                    [pe],                           #h_gps_e
+                    [np.sqrt(Ve**2 + Vn**2)],    #h_gps_Vg
+                    [np.arctan2(Ve,Vn)]])           #h_chi
+        return h_
 
-    def jacobian(fun, x, u_):
+    def jacobian(self, fun, x, u_):
         # compute jacobian of fun with respect to x
         f = fun(x, u_)
         m = f.shape[0]
